9b59f408ace5f24c332182a7bee00db23ba7b331
Jager Ente <JustlPains@gmail.com>
2020-12-14 13:47:27 +0300

    Added some stuff for Guides module

---
+++ Modules/Guides/Category.cs
@@ -0,0 +1,22 @@
+﻿namespace LisaBot.Modules.Guides
+{
+    public class Category
+    {
+        public string Title { get; set; }
+        public Guide[] List { get; set; }
+        
+    }
+    public class Guide
+    {
+        public string Title { get; set; }
+        public Info Info { get; set; }
+    }
+
+    public class Info
+    {
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public bool Premium { get; set; }
+        public bool EarlyAccess { get; set; }
+    }
+}
