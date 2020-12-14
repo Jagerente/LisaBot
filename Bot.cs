23ae7731389a632c4faba2d214bd4c368407f56c
Jager Ente <JustlPains@gmail.com>
2020-12-14 13:45:20 +0300

    Added some logs

--- Bot.cs
+++ Bot.cs
@@ -1,22 +1,28 @@
 ﻿using DSharpPlus;
 using DSharpPlus.EventArgs;
 using DSharpPlus.CommandsNext;
+using LisaBot.Commands;
 using System;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
 
 namespace LisaBot
 {
     class Bot
     {
+        public const string BotName = "LisaBot";
         public DiscordClient Client { get; private set; }
-        public CommandsNextModule Commands { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
 
         public  async Task RunAsync()
         {
             var json = string.Empty;
+
             if (!File.Exists("config.json"))
             {
                 var jsobj = new JsonStorage();
@@ -39,32 +45,62 @@ namespace LisaBot
                 Token = configJson.Token,
                 TokenType = TokenType.Bot,
                 AutoReconnect  = true,
-                LogLevel = LogLevel.Debug,
-                UseInternalLogHandler = true
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug
             };
 
             Client = new DiscordClient(config);
 
             Client.Ready += OnClientReady;
 
+            Client.MessageCreated += OnMessageCreated;
+
+            Client.GuildAvailable += OnGuildAvailable;
+
+            Client.UseInteractivity(new InteractivityConfiguration());
+
             var commandsConfig = new CommandsNextConfiguration
             {
-                StringPrefix = configJson.Prefix,
+                StringPrefixes = new string[] { configJson.Prefix },
                 EnableMentionPrefix = true,
                 EnableDms = true,
-                IgnoreExtraArguments = true
+                CaseSensitive = false
             };
 
             Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<OtherCommands>();
+
+            //Commands.RegisterCommands<GuideCommands>();
+
+            Commands.CommandExecuted += OnCommandExecuted;
 
             await Client.ConnectAsync();
 
             await Task.Delay(-1);
         }
 
-        private Task OnClientReady(ReadyEventArgs e)
+        private async Task OnGuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
         {
-            return Task.CompletedTask;
+            Console.WriteLine($"Guild available: {e.Guild.Name}");
+            await Task.CompletedTask;
+        }
+
+        private async Task OnMessageCreated(DiscordClient sender, MessageCreateEventArgs e)
+        {
+            Console.WriteLine($"{e.Guild.Name}/#{e.Channel.Name.TrimStart("Channel".ToCharArray())}/{e.Author.Username}: {e.Message.Content}");
+            await Task.CompletedTask;
+        }
+
+        private async Task OnCommandExecuted(CommandsNextExtension sender, CommandExecutionEventArgs e)
+        {
+            Console.WriteLine($"{e.Context.Guild.Name}/#{e.Context.Channel.Name.TrimStart("Channel".ToCharArray())}/{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'");
+            await Task.CompletedTask;
+        }
+
+        private async Task OnClientReady(DiscordClient sender, ReadyEventArgs e)
+        {
+            Console.WriteLine($"{BotName} is ready");
+            await Task.CompletedTask;
         }
     }
 }
