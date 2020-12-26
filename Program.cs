ca684f5080649960a41986eaed02d10aa32faa01
Jager Ente <JustlPains@gmail.com>
2020-12-25 19:51:54 -0800

    Updated

--- Program.cs
+++ Program.cs
@@ -1,55 +1,31 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Text;
+using System.Linq;
 
 namespace LisaBot
 {
     class Program
     {
-        private const bool Debug = true;
         static void Main(string[] args)
         {
-            if (!Debug)
-            {
-                var bot = new Bot();
+
+
+#if DEBUG
+            DoDebug();
+#else
+
+            var bot = new Bot();
                 bot.RunAsync().GetAwaiter().GetResult();
-            }
-            else
-            {
-                DoDebug();
-            }
+#endif
+
         }
+
 
         static void DoDebug()
         {
-            Console.WriteLine(Directory.GetCurrentDirectory());
-            Modules.Guides.Category[] cats =
-                JsonConvert.DeserializeObject<Modules.Guides.Category[]>("guides.json");
-            Console.WriteLine(cats[0].Title);
 
-
-            //var cat = new Modules.Guides.Category();
-            //cat.Title = "title";
-            //cat.List = new Modules.Guides.Guide[]
-            //{
-            //    new Modules.Guides.Guide
-            //    {
-            //        Title = "title",
-            //        Info = new Modules.Guides.Info
-            //        {
-            //            Link = "localhost",
-            //            Description = "descy",
-            //            Premium  = false,
-            //            EarlyAccess = true
-            //        }
-            //    }
-            //};
-            //var arr = new Modules.Guides.Category[]
-            //{
-            //    cat
-            //};
-
-            //new JsonStorage().StoreObject(arr, $@"{Directory.GetCurrentDirectory()}\guides.json");
         }
     }
 }
